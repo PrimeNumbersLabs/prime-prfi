e40b6c5cff22c30e1b5c178378cff6079043461a
Rainer Ramirez <rainer@primenumbers.xyz>
2025-02-20 19:11:55 -0600

    Add Layerzero config

--- layerzero.config.ts
+++ layerzero.config.ts
@@ -2,58 +2,209 @@ import { EndpointId } from '@layerzerolabs/lz-definitions'
 
 import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
 
+const ethereumContract: OmniPointHardhat = {
+    eid: EndpointId.ETHEREUM_V2_MAINNET,
+    contractName: 'PRFI',
+}
+
 const sepoliaContract: OmniPointHardhat = {
     eid: EndpointId.SEPOLIA_V2_TESTNET,
     contractName: 'PRFI',
 }
 
-const fujiContract: OmniPointHardhat = {
-    eid: EndpointId.AVALANCHE_V2_TESTNET,
+const BaseSepoliaContract: OmniPointHardhat = {
+    eid: EndpointId.BASESEP_V2_TESTNET,
     contractName: 'PRFI',
 }
 
-const amoyContract: OmniPointHardhat = {
-    eid: EndpointId.AMOY_V2_TESTNET,
+const BaseContract: OmniPointHardhat = {
+    eid: EndpointId.BASE_V2_MAINNET,
     contractName: 'PRFI',
 }
+
+const ArbitrumSepoliaContract: OmniPointHardhat = {
+    eid: EndpointId.ARBSEP_V2_TESTNET,
+    contractName: 'PRFI',
+}
+
+const ArbitrumContract: OmniPointHardhat = {
+    eid: EndpointId.ARBITRUM_V2_MAINNET,
+    contractName: 'PRFI',
+}
+
+const hyperliquidContract: OmniPointHardhat = {
+    eid: EndpointId.HYPERLIQUID_V2_MAINNET,
+    contractName: 'PRFI',
+}
+
+const HyperliquidTestnetContract: OmniPointHardhat = {
+    eid: EndpointId.HYPERLIQUID_V2_TESTNET,
+    contractName: 'PRFI',
+}
+
+const XDCContract: OmniPointHardhat = {
+    eid: EndpointId.XDC_V2_MAINNET,
+    contractName: 'PRFI',
+}
+
+
 
 const config: OAppOmniGraphHardhat = {
     contracts: [
         {
-            contract: fujiContract,
+            contract: ethereumContract,
         },
         {
             contract: sepoliaContract,
         },
         {
-            contract: amoyContract,
+            contract: BaseContract,
+        },
+        {
+            contract: BaseSepoliaContract,
+        },
+        {
+            contract: ArbitrumSepoliaContract,
+        },
+        {
+            contract: ArbitrumContract,
+        },
+        {
+            contract: hyperliquidContract,
+        },
+        {
+            contract: HyperliquidTestnetContract,
+        },
+        {
+            contract: XDCContract,
         },
     ],
     connections: [
+        // Ethereum
         {
-            from: fujiContract,
-            to: sepoliaContract,
+            from: ethereumContract,
+            to: BaseContract,
         },
         {
-            from: fujiContract,
-            to: amoyContract,
+            from: ethereumContract,
+            to: ArbitrumContract,
+        },
+        {
+            from: ethereumContract,
+            to: hyperliquidContract,
+        },
+        {
+            from: ethereumContract,
+            to: XDCContract,
+        },
+        // Arbitrum
+        {
+            from: ArbitrumContract,
+            to: ethereumContract,
+        },
+        {
+            from: ArbitrumContract,
+            to: BaseContract,
+        },
+        {
+            from: ArbitrumContract,
+            to: hyperliquidContract,
+        },
+        {
+            from: ArbitrumContract,
+            to: XDCContract,
+        },
+        // Hyperliquid
+        {
+            from: hyperliquidContract,
+            to: ethereumContract,
+        },
+        {
+            from: hyperliquidContract,
+            to: BaseContract,
+        },
+        {
+            from: hyperliquidContract,
+            to: ArbitrumContract,
+        },
+        {
+            from: hyperliquidContract,
+            to: XDCContract,
+        },
+        // XDC
+        {
+            from: XDCContract,
+            to: ethereumContract,
+        },
+        {
+            from: XDCContract,
+            to: BaseContract,
+        },
+        {
+            from: XDCContract,
+            to: ArbitrumContract,
+        },
+        {
+            from: XDCContract,
+            to: hyperliquidContract,
+        },
+        // Base
+        {
+            from: BaseContract,
+            to: ethereumContract,
+        },
+        {
+            from: BaseContract,
+            to: ArbitrumContract,
+        },
+        {
+            from: BaseContract,
+            to: hyperliquidContract,
+        },
+        {
+            from: BaseContract,
+            to: XDCContract,
+        },
+        //Tesnets Networks
+        // Sepolia
+        {
+            from: sepoliaContract,
+            to: BaseSepoliaContract,
         },
         {
             from: sepoliaContract,
-            to: fujiContract,
+            to: ArbitrumSepoliaContract,
         },
         {
             from: sepoliaContract,
-            to: amoyContract,
+            to: HyperliquidTestnetContract,
+        },
+        // Base Sepolia
+        {
+            from: BaseSepoliaContract,
+            to: ArbitrumSepoliaContract,
         },
         {
-            from: amoyContract,
+            from: BaseSepoliaContract,
+            to: HyperliquidTestnetContract,
+        },
+        {
+            from: BaseSepoliaContract,
             to: sepoliaContract,
         },
+        // Hyperliquid Testnet
         {
-            from: amoyContract,
-            to: fujiContract,
+            from: HyperliquidTestnetContract,
+            to: ArbitrumSepoliaContract,
         },
+        {
+            from: HyperliquidTestnetContract,
+            to: BaseSepoliaContract,
+        },
+        {
+            from: HyperliquidTestnetContract,
+            to: sepoliaContract,
+        }
     ],
 }
 
