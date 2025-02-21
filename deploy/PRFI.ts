d66a23d2ee3ae8087bc9dfb4a1f04906ceea3a07
Rainer Ramirez <rainer@primenumbers.xyz>
2025-02-20 18:16:40 -0600

    Deterministic Address

--- deploy/PRFI.ts
+++ deploy/PRFI.ts
@@ -1,10 +1,14 @@
 import assert from 'assert'
 
 import { type DeployFunction } from 'hardhat-deploy/types'
+import { bytecode } from "../artifacts/contracts/PRFI.sol/PRFI.json";
+const { encoder, create2Address } = require("../utils.js")
+import { ethers } from 'hardhat';
 
 const contractName = 'PRFI'
 
 const deploy: DeployFunction = async (hre) => {
+
     const { getNamedAccounts, deployments } = hre
 
     const { deploy } = deployments
@@ -15,37 +19,25 @@ const deploy: DeployFunction = async (hre) => {
     console.log(`Network: ${hre.network.name}`)
     console.log(`Deployer: ${deployer}`)
 
-    // This is an external deployment pulled in from @layerzerolabs/lz-evm-sdk-v2
-    //
-    // @layerzerolabs/toolbox-hardhat takes care of plugging in the external deployments
-    // from @layerzerolabs packages based on the configuration in your hardhat config
-    //
-    // For this to work correctly, your network config must define an eid property
-    // set to `EndpointId` as defined in @layerzerolabs/lz-definitions
-    //
-    // For example:
-    //
-    // networks: {
-    //   fuji: {
-    //     ...
-    //     eid: EndpointId.AVALANCHE_V2_TESTNET
-    //   }
-    // }
     const endpointV2Deployment = await hre.deployments.get('EndpointV2')
+   
+    const factoryAddr = "0xAC2A150065090a58C1a325341d66A28cA1e24289";
+    const saltHex = ethers.utils.id("1234");
+    const initCode = bytecode + encoder(["string", "string", "address", "address"], ["PRFI", "PRFI", endpointV2Deployment.address, deployer]);
 
-    const { address } = await deploy(contractName, {
-        from: deployer,
-        args: [
-            'PRFI', // name
-            'PRFI', // symbol
-            endpointV2Deployment.address, // LayerZero's EndpointV2 address
-            deployer, // owner
-        ],
-        log: true,
-        skipIfAlreadyDeployed: false,
-    })
+    const create2Addr = create2Address(factoryAddr, saltHex, initCode);
+    console.log("precomputed address:", create2Addr);
 
-    console.log(`Deployed contract: ${contractName}, network: ${hre.network.name}, address: ${address}`)
+  
+    const Factory = await ethers.getContractFactory("DeterministicDeployFactory");
+    const factory = await Factory.attach(factoryAddr);
+
+    const prfiDeploy = await factory.deploy(initCode, saltHex);
+    const txReceipt = await prfiDeploy.wait();
+    console.log("Deployed to:", txReceipt);
+
+    
+    console.log(`Deployed contract: ${contractName}, network: ${hre.network.name}, address: ${txReceipt.events[0].args[0]}`)
 }
 
 deploy.tags = [contractName]
