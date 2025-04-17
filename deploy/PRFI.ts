0a46616d41e2ee361c4f1eb93f189113be87cfa0
Rainer Ramirez <rainer@primenumbers.xyz>
2025-04-17 10:59:31 -0600

    Deploy update

--- deploy/PRFI.ts
+++ deploy/PRFI.ts
@@ -21,10 +21,9 @@ const deploy: DeployFunction = async (hre) => {
 
     const endpointV2Deployment = await hre.deployments.get('EndpointV2')
    
-    const factoryAddr = "0x7a5d364b97126600C0AdDFD5C339230748bcaA17";
-    const factoryAddrXDCMainnet = "0x2AE9Ec39093998a09266f275AFF7Ef37d242210E";
+    const factoryAddr = "0x8424F59a7D6f6270E44F760982ddBAC2c3d7cb81";
 
-    const saltHex = ethers.utils.id("777");
+    const saltHex = ethers.utils.id("12345");
     const initCode = bytecode + encoder(["string", "string", "address", "address"], ["PRFI", "PRFI", endpointV2Deployment.address, deployer]);
 
     const create2Addr = create2Address(factoryAddr, saltHex, initCode);
